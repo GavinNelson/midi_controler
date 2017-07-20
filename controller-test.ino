1e648aaf7c20f18bb75b5f6d7387749ac638ab1f
gavinn <gavin.nelson1@googlemail.com>
2017-07-20 22:49:39 +0100

    use 32 controls

--- controller-test.ino
+++ controller-test.ino
@@ -1,3 +1,5 @@
+1#include <MIDI.h>
+
  /*
  * codeexample for useing a 4051 * analog multiplexer / demultiplexer
  * by david c. and tomek n.* for k3 / malmï¿½ hï¿½gskola
@@ -13,60 +15,63 @@ int r2 = 0;      //value of select pin at the 4051 (s2)
 int s0 = 10;//purple
 int s1 = 11;//orange
 int s2 = 8;//blue
-int count = 0;   //which y pin we are selecting
+int ctrlcount = 0;  //which pot
 int val;
-int vals[8];
-int aPin = 21;
+int vals[32];
+int aPins[] = {21, 20, 19, 18};
 bool change = false;
-int acount;
 
 void setup(){  
   pinMode(s0, OUTPUT);
   pinMode(s1, OUTPUT);
   pinMode(s2, OUTPUT);
-  pinMode(aPin, INPUT);
+  for (int i = 0; i < sizeof(aPins) - 1; i ++) {
+    pinMode(aPins[i], INPUT);
+  }
 }
 
 void loop () {
 
-  for (count=0; count<=7; count++) {
+  for (int muxcount = 0; muxcount <= 7; muxcount++) {
 
-    // select the bit  
-    r0 = bitRead(count,0);    // use this with arduino 0013 (and newer versions)     
-    r1 = bitRead(count,1);    // use this with arduino 0013 (and newer versions)     
-    r2 = bitRead(count,2);    // use this with arduino 0013 (and newer versions)     
-
-    //r0 = count & 0x01;      // old version of setting the bits
-    //r1 = (count>>1) & 0x01;      // old version of setting the bits
-    //r2 = (count>>2) & 0x01;      // old version of setting the bits
-
-    digitalWrite(s0, r0);
-    digitalWrite(s1, r1);
-    digitalWrite(s2, r2);
-
-    //Either read or write the multiplexed pin here
-    val = analogRead(aPin)/8;
+    setMuxChannel(muxcount);
     
-    if (count == 0) {
+    if (muxcount == 0) {
       if (change == true) {
-        printArray(vals);
+        printVals();
       }
       change = false;
+      ctrlcount = 0;
     }
-    if (vals[count] != val) {
-//    if ((vals[count] + 1) < val || (vals[count] - 1) > val) {
-      change = true;
-      vals[count] = val;
+    for (int incount = 0; incount <= sizeof(aPins) - 1; incount++) {
+      val = analogRead(aPins[incount]);
+      if (vals[ctrlcount] != val) {
+  //    if (vals[count] + 1 < val || vals[count) - 1 > val || val = 1023) {
+        change = true;
+        vals[ctrlcount] = val;
+        ctrlcount++;
+      }
     }
   }  
 }
 
 
-void printArray(int arr[]) {
-  for (acount = 0; acount < 7; acount++) {
-    Serial.print(arr[acount]);
+void printVals() {
+  for (int i = 0; i < sizeof(vals) - 1; i++) {
+    Serial.print(vals[i]);//bitsi
     Serial.print("\t");
   }
   Serial.print("\n");
+}
+
+void setMuxChannel(int ch) {
+      // select the bit
+    r0 = bitRead(ch,0);
+    r1 = bitRead(ch,1);
+    r2 = bitRead(ch,2);
+
+    digitalWrite(s0, r0);
+    digitalWrite(s1, r1);
+    digitalWrite(s2, r2);
 }
 
